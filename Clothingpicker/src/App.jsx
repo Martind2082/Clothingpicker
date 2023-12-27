85a96b0e60027f6f6152cb274cd8dee32c703d4f
Martin <martindao2082@gmail.com>
2023-12-26 23:35:47 -0800

    done with generate outfit section and added buttons to add a clothing item from wardrobe into outfit

--- Clothingpicker/src/App.jsx
+++ Clothingpicker/src/App.jsx
@@ -311,7 +311,6 @@ function App() {
     return shuffled;
   }
   async function getweatheroutfit() {
-    console.log("weather outfit ran");
     if (User) {
       setTimeout(() => {
         let temp = temperature.current.textContent.split('');
@@ -509,8 +508,92 @@ function App() {
   getweatheroutfit()
 
   //generate a random outfit
+  const generatejacket = useRef();
+  const generateshirt = useRef();
+  const generatepants = useRef();
+  const generateshoes = useRef();
+  const generatemissingclothes = useRef();
   function generateoutfit() {
+      let missingarr = [];
+      let jacketarr = [];
+      onSnapshot(collection(db, `${User.email}jacket`), (snapshot) => {
+        snapshot.docs.forEach(jacket => {
+          jacketarr.push(jacket.data());
+        })
+        let shuffledarr = shuffle(jacketarr);
+        let num = Math.floor(Math.random() * shuffledarr.length)
+        if (shuffledarr.length == 0) {
+          generatejacket.current.src = "https://wallpapers.com/images/featured/blank-white-7sn5o1woonmklx1h.jpg";
+          missingarr.push("jacket/hoodie");
+        } else {
+          generatejacket.current.src = shuffledarr[num].url;
+        }
+      })
 
+      let shirtsarr = [];
+      onSnapshot(collection(db, `${User.email}shirt`), (snapshot) => {
+        snapshot.docs.forEach(shirt => {
+            shirtsarr.push(shirt.data());
+        })
+        let shuffledarr = shuffle(shirtsarr);
+        let num = Math.floor(Math.random() * shuffledarr.length);
+        if (shuffledarr.length == 0) {
+          generateshirt.current.src = "https://wallpapers.com/images/featured/blank-white-7sn5o1woonmklx1h.jpg";
+          missingarr.push("shirt");
+        } else {
+          generateshirt.current.src = shuffledarr[num].url;
+        }
+      })
+
+      let pantsarr = [];
+      onSnapshot(collection(db, `${User.email}pants`), (snapshot) => {
+        snapshot.docs.forEach(pant => {
+            pantsarr.push(pant.data());
+        })
+        let shuffledarr = shuffle(pantsarr);
+        let num = Math.floor(Math.random() * shuffledarr.length);
+        if (shuffledarr.length == 0) {
+          generatepants.current.src = "https://wallpapers.com/images/featured/blank-white-7sn5o1woonmklx1h.jpg";
+          missingarr.push("pants");
+        } else {
+          generatepants.current.src = shuffledarr[num].url;
+        }
+      })
+
+      let shoesarr = [];
+      onSnapshot(collection(db, `${User.email}shoe`), (snapshot) => {
+        snapshot.docs.forEach(shoe => {
+            shoesarr.push(shoe.data());
+        })
+        let shuffledarr = shuffle(shoesarr);
+        let num = Math.floor(Math.random() * shuffledarr.length);
+        if (shuffledarr.length == 0) {
+          generateshoes.current.src = "https://wallpapers.com/images/featured/blank-white-7sn5o1woonmklx1h.jpg";
+          missingarr.push("shoes");
+        } else {
+          generateshoes.current.src = shuffledarr[num].url;
+        }
+      })
+
+      setTimeout(() => {
+        if (missingarr.length !== 0) {
+          missingarr = missingarr.join(', ');
+          generatemissingclothes.current.textContent = `You are missing ${missingarr}`;
+        }
+      }, 500);
+  }
+
+  //adds selected clothing item to outfit
+  function addtooutfit(item) {
+    if (item.data().type == "jacket") {
+      generatejacket.current.src = item.data().url;
+    } else if (item.data().type == "shirt") {
+      generateshirt.current.src = item.data().url;
+    } else if (item.data().type == "pants") {
+      generatepants.current.src = item.data().url;
+    } else {
+      generateshoes.current.src = item.data().url;
+    }
   }
 
   return (
@@ -600,7 +683,7 @@ function App() {
                   <div className='w-full h-[80vh] flex mt-8 border-t-[3px] border-t-gray-400'>
                     <div className='w-2/4 border-r-[3px] border-gray-400 relative'>
                       <p className='text-[1.5rem] text-center mt-4'>Outfit recommended for current weather conditions</p>
-                      <div ref={weatheroutfit} className='text-[2rem] px-8 pt-8 text-center w-full h-[65vh] bg-gray-100 rounded-lg mt-4 absolute flex flex-col items-center'>
+                      <div ref={weatheroutfit} className='text-[2rem] px-8 py-4 pt-8 text-center w-full h-[65vh] bg-gray-100 rounded-lg mt-4 absolute flex flex-col items-center'>
                         <p ref={weatheroutfitmessage} className='text-[1.3rem] mb-2'></p>
                         <p ref={weatheroutfitmessage2} className='text-[1.3rem] mb-[1.5rem]'></p>
                         <img className='w-[6rem] h-[6rem]' ref={weatherjacket}/>
@@ -611,7 +694,14 @@ function App() {
                     <div className='w-2/4 flex items-center flex-col'>
                       <p className='text-[1.5rem] text-center mt-4'>My Outfit</p>
                       <p className='text-center text-[1.2rem]'>Pick out your outfit in the wardrobe below!</p>
-                      <button onClick={generateoutfit} className='px-8 py-[3px] border border-black rounded-[10px] w-[80%] mt-4 bg-gradient-to-r from-green-400 to-green-500'>Can't Decide? Generate a random outfit!</button>
+                      <button onClick={generateoutfit} className='px-8 py-[3px] border border-black rounded-[10px] w-[80%] mt-4 mb-2 bg-gradient-to-r from-green-400 to-green-500'>Can't Decide? Generate a random outfit!</button>
+                      <p ref={generatemissingclothes}></p>
+                      <div className='mt-8 bg-gray-100 rounded-lg w-full py-8 flex flex-col items-center'>
+                        <img className='w-[6rem] h-[6rem]' ref={generatejacket}/>
+                        <img className='w-[6rem] h-[6rem]' ref={generateshirt}/>
+                        <img className='w-[6rem] h-[6rem]' ref={generatepants}/>
+                        <img className='w-[6rem] h-[6rem]' ref={generateshoes}/>
+                      </div>
                     </div>
                   </div>
 
@@ -630,8 +720,9 @@ function App() {
                           shoesarr.map(shoe => {
                             return <SwiperSlide className='w-full h-full slide'  id={shoe.id}>
                               <img className='w-full h-full object-contain' src={shoe.data().url}/>
-                              <div className='clothingoptions'>
+                              <div className='clothingoptions flex flex-col items-center'>
                                 <button onClick={() => deleteclothing(shoe)} className='deleteclothing bg-white h-min px-3 rounded-md border border-black mt-2'>Delete</button>
+                                <button onClick={() => addtooutfit(shoe)} className='bg-white h-min px-3 rounded-md border border-black mt-2'>Add to my Outfit</button>
                               </div>
                             </SwiperSlide>
                           })
@@ -645,8 +736,9 @@ function App() {
                         jacketsarr.map(jacket => {
                           return <SwiperSlide className='w-[3rem] h-[3rem] slide'  id={jacket.id}>
                             <img className='w-full h-full object-contain' src={jacket.data().url}/>
-                            <div className='clothingoptions'>
-                              <button onClick={() => deleteclothing(jacket)} className='deleteclothing bg-white h-min px-3 rounded-md border border-black mt-2'>Delete</button>
+                            <div className='clothingoptions flex flex-col items-center'>
+                              <button onClick={() => deleteclothing(jacket)} className='deleteclothing bg-white h-min px-3 rounded-md border border-black'>Delete</button>
+                              <button onClick={() => addtooutfit(jacket)} className='bg-white h-min px-3 rounded-md border border-black mt-2'>Add to my Outfit</button>
                             </div>
                           </SwiperSlide>
                         })
@@ -660,8 +752,9 @@ function App() {
                           shirtsarr.map(shirt => {
                             return <SwiperSlide className='w-[3rem] h-[3rem] slide'  id={shirt.id}>
                               <img className='w-full h-full object-contain' src={shirt.data().url}/>
-                              <div className='clothingoptions'>
+                              <div className='clothingoptions flex flex-col items-center'>
                                 <button onClick={() => deleteclothing(shirt)} className='deleteclothing bg-white h-min px-3 rounded-md border border-black mt-2'>Delete</button>
+                              <button onClick={() => addtooutfit(shirt)} className='bg-white h-min px-3 rounded-md border border-black mt-2'>Add to my Outfit</button>
                               </div>
                             </SwiperSlide>
                           })
@@ -675,8 +768,9 @@ function App() {
                           pantsarr.map(pant => {
                             return <SwiperSlide className='w-[3rem] h-[3rem] slide'  id={pant.id}>
                               <img className='w-full h-full object-contain' src={pant.data().url}/>
-                              <div className='clothingoptions'>
+                              <div className='clothingoptions flex flex-col items-center'>
                                 <button onClick={() => deleteclothing(pant)} className='deleteclothing bg-white h-min px-3 rounded-md border border-black mt-2'>Delete</button>
+                                <button onClick={() => addtooutfit(pant)} className='bg-white h-min px-3 rounded-md border border-black mt-2'>Add to my Outfit</button>
                               </div>
                             </SwiperSlide>
                           })
